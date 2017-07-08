bf749de4806b93e1009eb9a9e151e623da34ff3c
avoup <avoup.g@gmail.com>
2017-07-08 20:05:35 +0400

    ajax butt

--- views/js/ajax.js
+++ views/js/ajax.js
@@ -1,29 +1,44 @@
-$(document).ready(function() {
+// $(document).ready(function() {
+//
+//     $('#vform').submit(function(event) {
+//
+//         var formData = {
+//             'name'              : $('input[name=username]').val(),
+//             'email'             : $('input[name=email]').val(),
+//             'password'          : $('input[name=password]').val(),
+//             'repeated_password' : $('input[name=repeated_password]').val(),
+//             'cityid'            : $('input[name=cityid]').val()
+//         };
+//
+//         $.ajax({
+//             type        : 'GET',
+//             url         : 'index.php',
+//             data        : formData,
+//             dataType    : 'json',
+//                         encode          : true
+//         })
+//             .done(function(data) {
+//
+//                 console.log(data);
+//
+//             });
+//
+//         event.preventDefault();
+//     });
+//
+// });
 
-    $('#vform').submit(function(event) {
 
-        var formData = {
-            'name'              : $('input[name=username]').val(),
-            'email'             : $('input[name=email]').val(),
-            'password'          : $('input[name=password]').val(),
-            'repeated_password' : $('input[name=repeated_password]').val(),
-            'cityid'            : $('input[name=cityid]').val()
-        };
+$( "#butt" ).click(function() {
 
-        $.ajax({
-            type        : 'POST',
-            url         : 'index.php',
-            data        : formData,
-            dataType    : 'json',
-                        encode          : true
-        })
-            .done(function(data) {
-
-                console.log(data);
-
-            });
-
-        event.preventDefault();
-    });
+  jQuery.ajax({
+      url: '/some/endpoint.php',
+      method: 'GET',
+      data: $('#the-form').serialize()
+  }).done(function (response) {
+     alert("asld;fkjalsdh") 
+  }).fail(function () {
+      alert("error")
+  });
 
 });
