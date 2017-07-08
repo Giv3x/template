1cce00c71a0063f627f6e2a3135aec3c7491a161
avoup <avoup.g@gmail.com>
2017-07-08 22:58:37 +0400

    working ajax :D

--- views/js/ajax.js
+++ views/js/ajax.js
@@ -1,44 +1,23 @@
-// $(document).ready(function() {
-//
-//     $('#vform').submit(function(event) {
-//
-//         var formData = {
-//             'name'              : $('input[name=username]').val(),
-//             'email'             : $('input[name=email]').val(),
-//             'password'          : $('input[name=password]').val(),
-//             'repeated_password' : $('input[name=repeated_password]').val(),
-//             'cityid'            : $('input[name=cityid]').val()
-//         };
-//
-//         $.ajax({
-//             type        : 'GET',
-//             url         : 'index.php',
-//             data        : formData,
-//             dataType    : 'json',
-//                         encode          : true
-//         })
-//             .done(function(data) {
-//
-//                 console.log(data);
-//
-//             });
-//
-//         event.preventDefault();
-//     });
-//
-// });
-
-
-$( "#butt" ).click(function() {
-
-  jQuery.ajax({
-      url: 'index.php',
-      method: 'GET',
-      data: $('#vform').serialize()
-  }).done(function (response) {
-     alert("asld;fkjalsdh")
-  }).fail(function () {
-      alert("error")
+$(function() {
+  $(".button").click(function() {
+      $.ajax({
+        url: 'loader.php?a=UserController.User.register',
+        type: "post",
+        data:  $('#vform').serialize(),
+        success: function(response) {
+          console.log(response);
+          $('#submitted').html("<div id='message'></div>");
+          $('#message').html("<h2>Contact Form Submitted!</h2>")
+          .append("<p>hasta la vista bitch</p>")
+          .hide()
+          .fadeIn(1500, function() {
+            $('#message')
+          });
+        },
+        error: function(error){
+          console.log(error);
+        }
+      });
+    return false;
   });
-
 });
